9e078ad3cef6b8d871400ebe8124960ec2d47bc9
Hania Said <haniasaid110@gmail.com>
2026-04-11 00:38:09 +0200

    feat: configure homepage URL and implement react-router routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
